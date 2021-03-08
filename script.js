51d4173a28235e98cba0684e5ebcb5865d3b23fb
hayden <hmjoyce451@gmail.com>
2021-03-08 15:00:51 -0800

    Revisiting this project to add DOM elements. Still much to do with styling to make it look better, but the javaScript is functional

--- script.js
+++ script.js
@@ -1,69 +1,82 @@
-const choices = ['rock', 'paper', 'scissors'];
-
-let computerSelection;
-let userSelection;
-
+//scoring elements
 let userScore = 0;
 let computerScore = 0;
+//user button choices
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper')
+const scissorsBtn = document.getElementById('scissors');
+//display score on dom
+const userScoreDisplay = document.querySelector('.user-score');
+const computerScoreDisplay = document.querySelector('.computer-score');
+const result = document.querySelector('.description-text');
 
+const choices_div = document.querySelector('.choices');
+//get computer selection
 function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
     let random = Math.floor(Math.random() * 3);
     return choices[random];
 }
 
-function playRound(userSelection, computerSelection) {
-   userSelection = prompt('Rock, paper or scissors? ').toLowerCase();
-   computerSelection = computerPlay();
-    if(userSelection === computerSelection) {
-       console.log(`computer chooses ${computerSelection}`);
-       console.log(`user chooses ${userSelection}`);
-       return `${userSelection} ties ${computerSelection}.  It's a draw!`;
-   } else if (userSelection === 'rock' && computerSelection === 'paper') {
-    console.log(`computer chooses ${computerSelection}`);
-    console.log(`user chooses ${userSelection}`);
-    computerScore++;
-    console.log(userScore, computerScore);
-    return `${computerSelection} defeats ${userSelection}. You Lose!`;
-   } else if(userSelection === 'rock' && computerSelection === 'scissors') {
-    console.log(`computer chooses ${computerSelection}`);
-    console.log(`user chooses ${userSelection}`);
-    userScore++;
-    console.log(userScore, computerScore);
-    return `${userSelection} defeats ${computerSelection}. You Win!`;
-   } else if(userSelection === 'paper' && computerSelection === 'rock') {
-    console.log(`computer chooses ${computerSelection}`);
-    console.log(`user chooses ${userSelection}`);
-    userScore++;
-    console.log(userScore, computerScore);
-    return `${userSelection} defeats ${computerSelection}. You Win!`;
-   } else if(userSelection === 'paper' && computerSelection === 'scissors') {
-    console.log(`computer chooses ${computerSelection}`);
-    console.log(`user chooses ${userSelection}`);
-    computerScore++;
-    console.log(userScore, computerScore);
-    return `${computerSelection} defeats ${userSelection}. You Lose!`;
-   } else if(userSelection === 'scissors' && computerSelection === 'rock') {
-    console.log(`computer chooses ${computerSelection}`);
-    console.log(`user chooses ${userSelection}`);
-    computerScore++;
-    console.log(userScore, computerScore);
-    return `${computerSelection} defeats ${userSelection}. You Lose!`;
-   } else if(userSelection === 'scissors' && computerSelection === 'paper') {
-    console.log(`computer chooses ${computerSelection}`);
-    console.log(`user chooses ${userSelection}`);
-    userScore++;
-    console.log(userScore, computerScore);
-    return `${userSelection} defeats ${computerSelection}. You Win!`;
-   }
-}
+//playRound
 
-function game () {
-    while(userScore < 5 && computerScore < 5) {
-        console.log(playRound());
-    } if(userScore >= 5) {
-        return `USER WINS!!!`
-    } else if (computerScore >= 5) {
-        return `Computer wins.... :(`;
+function playRound(userSelection) {
+    const computerSelection = computerPlay();
+    switch(userSelection + computerSelection) {
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+            draw(userSelection, computerSelection);
+            break;
+        case 'rockscissors':
+        case 'paperrock':
+        case 'scissorspaper':
+            win(userSelection, computerSelection);
+            break;
+        case 'rockpaper':
+        case 'paperscissors':
+        case 'scissorsrock':
+            lose(userSelection, computerSelection);
+            break;
     }
 }
-console.log(game());
+
+function win(userSelection, computerSelection) {
+    userScore++;
+    userScoreDisplay.innerText = userScore;
+    computerScoreDisplay.innerText = computerScore;
+    result.innerText = `${userSelection} defeats ${computerSelection}. User WINS!`;
+    if(userScore > 4) {
+        userWin();
+    }
+}
+function lose(userSelection, computerSelection) {
+    computerScore++;
+    userScoreDisplay.innerText = userScore;
+    computerScoreDisplay.innerText = computerScore;
+    result.innerText = `${computerSelection} defeats ${userSelection}. User LOSES!`;
+    if(computerScore > 4) {
+        userLose();
+    }
+}
+function draw(userSelection, computerSelection) {
+    result.innerText = `${userSelection} ties ${computerSelection}. It's a DRAW!`;
+    userScoreDisplay.innerText = userScore;
+    computerScoreDisplay.innerText = computerScore;
+}
+
+function userWin() {
+        choices_div.classList.add('game-over');
+        result.innerText = 'GAME OVER, USER WINS!!!!'
+}
+function userLose() {
+    choices_div.classList.add('game-over');
+    result.innerText = 'GAME OVER, COMPUTER WINS... :('
+}
+
+function game() {
+    rockBtn.addEventListener('click', () => playRound('rock'));
+    paperBtn.addEventListener('click', () => playRound('paper'));
+    scissorsBtn.addEventListener('click', () => playRound('scissors'));
+}
+game();
